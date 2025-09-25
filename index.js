db5fe4b2cd38ff94df36d67eea4fe1ce9e9909b4
Douglas Ramos Pimenta <douglasramospimenta@gmail.com>
2025-09-25 20:59:57 -0300

    chore: change Postgres port

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 import 'dotenv/config.js'
 import express from 'express'
 
-const name = 'Diego'
-
 const app = express()
 
 app.use(express.json())
 
 app.listen(process.env.PORT, () => {
-    console.log('Server is running on port 3000')
+    console.log('Server is running on port 8080')
 })
