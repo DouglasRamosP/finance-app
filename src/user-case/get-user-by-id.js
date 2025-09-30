26413171973c35f7bf840a705429df66de41c0ac
Douglas Ramos Pimenta <douglasramospimenta@gmail.com>
2025-09-30 17:43:44 -0300

    feat: add GetUserByIdUseCase

---
+++ src/user-case/get-user-by-id.js
@@ -0,0 +1,10 @@
+import { PostgresGetUserRepository } from '../repositories/postgres/get-user-by-id.js'
+
+export class GetUserByIdUseCase {
+    async execute(userId) {
+        const getUserByIdRepository = new PostgresGetUserRepository()
+        const user = await getUserByIdRepository.execute(userId)
+
+        return user
+    }
+}
