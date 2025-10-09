2d7b65cf6172f97f9a8be2bf8073e4a15bc59235
Douglas Ramos Pimenta <douglasramospimenta@gmail.com>
2025-10-09 01:22:59 -0300

    fix: syntax correction

--- src/repositories/postgres/get-user-by-id.js
+++ src/repositories/postgres/get-user-by-id.js
@@ -1,9 +1,10 @@
-import { PostgresHelper } from "../../db/postgres/helper.js";
+import { PostgresHelper } from '../../db/postgres/helper.js'
 
 export class PostgresGetUserRepository {
     async execute(userId) {
-        const user = await PostgresHelper.query('SELECT * FROM users WHERE id = $1',
-            [userId]
+        const user = await PostgresHelper.query(
+            'SELECT * FROM users WHERE id = $1',
+            [userId],
         )
 
         return user[0]
