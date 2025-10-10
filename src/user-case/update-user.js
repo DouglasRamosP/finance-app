dac8b7a9c38e38e10cad8a533a7cc794756d9c4c
Douglas Ramos Pimenta <douglasramospimenta@gmail.com>
2025-10-10 16:47:54 -0300

    refactor: move user repositorie its own folder and refactor link route the import

--- src/user-case/update-user.js
+++ src/user-case/update-user.js
@@ -1,6 +1,6 @@
 import bcrypt from 'bcrypt'
 
-import { PostgresGetUserByEmailRepository } from '../repositories/postgres/get-user-by-email.js'
+import { PostgresGetUserByEmailRepository } from '../repositories/postgres/user/get-user-by-email.js'
 import { EmailAlreadyInUseError } from '../errors/user.js'
 
 export class UpdateUserUseCase {
