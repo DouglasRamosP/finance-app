3ce982d1a042aee2bad57b33b9b546277c7807a3
Douglas Ramos Pimenta <douglasramospimenta@gmail.com>
2025-10-17 15:26:48 -0300

    feat: add GetTransactionByUserIdController

---
+++ src/controllers/transaction/get-transaction-by-user-id.js
@@ -0,0 +1,41 @@
+import { UserNotFoundError } from '../../errors/user.js'
+import { ok, serverError } from '../helpers/http.js'
+import {
+    generateInvalidIdResponse,
+    requiredFildIsMissingResponse,
+    userNotFoundResponse,
+} from '../helpers/response.js'
+
+import { checkedIfIdIsValid } from '../helpers/validation.js'
+
+export class GetTransactionByUserIdController {
+    constructor(getTransactionByUserIdUseCase) {
+        this.getTransactionByUserIdUseCase = getTransactionByUserIdUseCase
+    }
+    async execute(httpRequest) {
+        try {
+            // Verificar se o userId foi passado como parametro
+            const userId = httpRequest.query.userId
+            if (!userId) {
+                return requiredFildIsMissingResponse('userId')
+            }
+            // Verificar se o userId é um ID válido
+            const userIdIsValid = checkedIfIdIsValid(userId)
+            if (!userIdIsValid) {
+                return generateInvalidIdResponse()
+            }
+            // Chamar Use Case
+            const getTransaction =
+                await this.getTransactionByUserIdUseCase.execute(userId)
+
+            return ok(getTransaction)
+            // retomar resposta http
+        } catch (error) {
+            console.error(error)
+            if (error instanceof UserNotFoundError) {
+                return userNotFoundResponse()
+            }
+            return serverError
+        }
+    }
+}
