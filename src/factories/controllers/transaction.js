45285349a8549f1655b91912c25290a86d2f2493
Douglas Ramos Pimenta <douglasramospimenta@gmail.com>
2025-10-17 15:26:11 -0300

    feat: add makerGetTransactionByUserIdController

--- src/factories/controllers/transaction.js
+++ src/factories/controllers/transaction.js
@@ -1,7 +1,10 @@
 import { CreateTransactionController } from '../../controllers/transaction/create-transaction.js'
+import { GetTransactionByUserIdController } from '../../controllers/transaction/get-transaction-by-user-id.js'
 import { PostgresCreateTransactionRepository } from '../../repositories/postgres/transaction/create-transaction.js'
+import { PostgresGetTransactionByUserIdRepository } from '../../repositories/postgres/transaction/get-transaction-by-user-id.js'
 import { PostgresGetUserRepository } from '../../repositories/postgres/user/get-user-by-id.js'
 import { CreateTransactionUseCase } from '../../user-case/transaction/create-transaction.js'
+import { GetTransactionByUserIdUseCase } from '../../user-case/transaction/get-transaction-by-use-case.js'
 
 export const makerCreateTransactionController = () => {
     const createTransactionRepository =
@@ -19,4 +22,20 @@ export const makerCreateTransactionController = () => {
     )
 
     return createTransactionController
+}
+
+export const makerGetTransactionByUserIdController = () => {
+    const getTransactionByUserIdRepository =
+        new PostgresGetTransactionByUserIdRepository()
+
+    const getUserRepository = new PostgresGetUserRepository()
+
+    const getTransactionByUserIdUseCase = new GetTransactionByUserIdUseCase(
+        getTransactionByUserIdRepository,
+        getUserRepository,
+    )
+
+    const getTransactionByUserIdController = new GetTransactionByUserIdController(getTransactionByUserIdUseCase)
+
+    return getTransactionByUserIdController
 }
