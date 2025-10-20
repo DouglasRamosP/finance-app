eca73a16df4dceca4d74a41e0be19846cb33d491
Douglas Ramos Pimenta <douglasramospimenta@gmail.com>
2025-10-20 11:35:12 -0300

    feat: add PostgresUpdateTransactionRepository

---
+++ src/repositories/postgres/transaction/update-transaction.js
@@ -0,0 +1,28 @@
+import { PostgresHelper } from '../../../db/postgres/helper.js'
+
+export class PostgresUpdateTransactionRepository {
+    async execute(userId, updateTrasactionParams) {
+        const updateFields = []
+        const updateValues = []
+
+        Object.keys(updateTrasactionParams).forEach((key) => {
+            updateFields.push(`${key} = $${updateValues.length + 1}`)
+            updateValues.push(updateTrasactionParams[key])
+        })
+
+        updateValues.push(userId)
+
+        const updateQuery = `
+        UPDATE transactions SET ${updateFields.join(',')}
+        WHERE id = $${updateValues.length}
+        RETURNING *
+        `
+
+        const updateTransaction = await PostgresHelper.query(
+            updateQuery,
+            updateValues,
+        )
+
+        return updateTransaction[0]
+    }
+}
