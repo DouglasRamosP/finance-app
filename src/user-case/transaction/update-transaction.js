3715a286b034aac453aebe334749f9b3aeaa83f7
Douglas Ramos Pimenta <douglasramospimenta@gmail.com>
2025-10-20 22:53:22 -0300

    fix: corrige clas name for UpdateTransactionUsecase

--- src/user-case/transaction/update-transaction.js
+++ src/user-case/transaction/update-transaction.js
@@ -1,4 +1,4 @@
-export class UpdateTransactionUsecase {
+export class UpdateTransactionUseCase {
     constructor(postgresUpdateTransactionRepository) {
         this.postgresUpdateTransactionRepository =
             postgresUpdateTransactionRepository
