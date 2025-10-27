4577f2c7ac55dbe675a3dbe9032c1fd2d48c8c64
Douglas Ramos Pimenta <douglasramospimenta@gmail.com>
2025-10-27 10:49:35 -0300

    feat: add zod to CreateTransactionController

--- src/controllers/transaction/create-transaction.js
+++ src/controllers/transaction/create-transaction.js
@@ -1,17 +1,6 @@
+import { ZodError } from 'zod'
+import { createTransactionSchema } from '../../schemas/transaction.js'
 import { badRequest, created, serverError } from '../helpers/http.js'
-import {
-    checkIfAmountIsValid,
-    checkIfTypeIsValid,
-} from '../helpers/validation.js'
-import {
-    generateInvalidIdResponse,
-    invalidAmountResponse,
-    requiredFildIsMissingResponse,
-} from '../helpers/response.js'
-import {
-    checkedIfIdIsValid,
-    checkedRequiredFields,
-} from '../helpers/validation.js'
 
 export class CreateTransactionController {
     constructor(createTransactionUseCase) {
@@ -20,50 +9,19 @@ export class CreateTransactionController {
     async execute(httpRequest) {
         try {
             const params = httpRequest.body
-            const requiredFields = ['user_id', 'name', 'date', 'amount', 'type']
 
-            const requiredFieldValidation = checkedRequiredFields(
-                params,
-                requiredFields,
-            )
+            await createTransactionSchema.parseAsync(params)
 
-            if (!requiredFieldValidation.ok) {
-                return requiredFildIsMissingResponse(requiredFieldValidation)
-            }
-
-            // validar o ID
-            const userIdIsValid = checkedIfIdIsValid(params.user_id)
-
-            if (!userIdIsValid) {
-                return generateInvalidIdResponse()
-            }
-
-            // validar o amount (utilizando BIB validaator) & validar amount <= 0
-            const amount = params.amount
-
-            const amountIsValid = checkIfAmountIsValid(amount)
-
-            if (!amountIsValid) {
-                return invalidAmountResponse()
-            }
-            // validar type (ESRNING, EXPENSE, INVESTMENT)
-            const type = params.type.trim().toUpperCase()
-
-            const typeIsValid = checkIfTypeIsValid(type)
-
-            if (!typeIsValid) {
-                return badRequest({
-                    message:
-                        'The type must be EARNINGS, EXPENSES or INVESTMENTS',
-                })
-            }
-            const transaction = await this.createTransactionUseCase.execute({
-                ...params,
-                type,
-            })
+            const transaction =
+                await this.createTransactionUseCase.execute(params)
 
             return created(transaction)
         } catch (error) {
+            if (error instanceof ZodError) {
+                return badRequest({
+                    message: error.issues[0].message,
+                })
+            }
             console.error(error)
             return serverError()
         }
---
+++ src/schemas/transaction.js
@@ -0,0 +1,23 @@
+import { z } from 'zod'
+import validator from 'validator'
+
+export const createTransactionSchema = z.object({
+    user_id: z.string().uuid(),
+    name: z.string().trim().min(1, {
+        message: 'Name is required.',
+    }),
+    date: z.coerce.date(),
+    amount: z
+        .number()
+        .min(1, {
+            message: 'Amount must be greater than 0.',
+        })
+        .refine((value) =>
+            validator.isCurrency(value.toFixed(2), {
+                digits_after_decimal: [2],
+                allow_negatives: false,
+                decimal_separator: '.',
+            }),
+        ),
+    type: z.enum(['EXPENSES', 'EARNINGS', 'INVESTMENTS']),
+})
