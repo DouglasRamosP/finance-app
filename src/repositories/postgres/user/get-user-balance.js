740b8c42c460710bad3694221de9afc6d056bdf9
Douglas Ramos Pimenta <douglasramospimenta@gmail.com>
2025-10-27 21:24:33 -0300

    feat: ensure PostgresGetUserBalanceRepository use prisma

--- src/repositories/postgres/user/get-user-balance.js
+++ src/repositories/postgres/user/get-user-balance.js
@@ -1,27 +1,54 @@
-import { PostgresHelper } from '../../../db/postgres/helper.js'
+import { prisma } from '../../../../prisma/prisma.js'
 
 export class PostgresGetUserBalanceRepository {
     async execute(userId) {
-        const userBalance = await PostgresHelper.query(
-            `
-            SELECT
-            SUM(CASE WHEN type = 'EARNINGS' THEN amount ELSE 0 END) AS earnings,
-            SUM(CASE WHEN type = 'EXPENSES' THEN amount ELSE 0 END) AS expenses,
-            SUM(CASE WHEN type = 'INVESTMENTS' THEN amount ELSE 0 END) AS investiments,
-            (
-            SUM(CASE WHEN type = 'EARNINGS' THEN amount ELSE 0 END)
-            - SUM(CASE WHEN type = 'EXPENSES' THEN amount ELSE 0 END)
-            - SUM(CASE WHEN type = 'INVESTMENTS' THEN amount ELSE 0 END)
-            ) AS  balance
-             FROM transactions
-             WHERE user_id = $1;
-            `,
-            [userId],
-        )
+        const {
+            _sum: { amount: totalExpenses },
+        } = await prisma.transactions.aggregate({
+            where: {
+                user_id: userId,
+                type: 'EXPENSES',
+            },
+            _sum: {
+                amount: true,
+            },
+        })
+
+        const {
+            _sum: { amount: totalEarnings },
+        } = await prisma.transactions.aggregate({
+            where: {
+                user_id: userId,
+                type: 'EARNINGS',
+            },
+            _sum: {
+                amount: true,
+            },
+        })
+
+        const {
+            _sum: { amount: totalIvestments },
+        } = await prisma.transactions.aggregate({
+            where: {
+                user_id: userId,
+                type: 'INVESTMENTS',
+            },
+            _sum: {
+                amount: true,
+            },
+        })
+
+        const _totalEarnings = totalEarnings || 0
+        const _totalExpenses = totalExpenses || 0
+        const _totalIvestments = totalIvestments || 0
+
+        const balance = _totalEarnings - _totalExpenses - _totalIvestments
 
         return {
-            userId,
-            ...userBalance[0],
+            earnings: _totalEarnings,
+            expenses: _totalExpenses,
+            investments: _totalIvestments,
+            balance,
         }
     }
 }
