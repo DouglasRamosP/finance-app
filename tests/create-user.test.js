c50e404b104d150210e1852f01417395993aa4fd
Douglas Ramos Pimenta <douglasramospimenta@gmail.com>
2025-10-29 11:23:30 -0300

    test: ensure 400 is returned if password is not provided

--- tests/create-user.test.js
+++ tests/create-user.test.js
@@ -85,4 +85,23 @@ describe('Create User Controller', () => {
         // assert
         expect(result.statusCode).toBe(400)
     })
+
+    it('should return 400 if password is not provided', async () => {
+        // arrange
+        const createUserUseCase = new CreateUserUseCaseStub()
+        const createUserController = new CreateUserController(createUserUseCase)
+        const httpRequest = {
+            body: {
+                first_name: 'Teste',
+                last_name: 'Testando',
+                email: 'teste@email.com',
+            },
+        }
+
+        // act
+        const result = await createUserController.execute(httpRequest)
+
+        // assert
+        expect(result.statusCode).toBe(400)
+    })
 })
