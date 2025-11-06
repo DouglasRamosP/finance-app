a43375abd7fa23f8a3944a6548af7229461091c6
Douglas Ramos Pimenta <douglasramospimenta@gmail.com>
2025-11-06 08:43:11 -0300

    test: ensure UpdateUserController returns 200 when update user successfully

---
+++ tests/update-user.test.js
@@ -0,0 +1,42 @@
+import { faker } from '@faker-js/faker'
+import { UpdateUserController } from '../src/controllers/user/update-user'
+
+describe('UpdateUserController', () => {
+    class UpdateUserUseCaseStub {
+        async execute() {
+            return {
+                id: faker.string.uuid(),
+                first_name: faker.person.firstName(),
+                last_name: faker.person.lastName(),
+                email: faker.internet.email(),
+                password: faker.internet.password({ length: 6 }),
+            }
+        }
+    }
+
+    const makeSut = () => {
+        const updateUserUseCase = new UpdateUserUseCaseStub()
+        const sut = new UpdateUserController(updateUserUseCase)
+
+        return { sut, updateUserUseCase }
+    }
+
+    const httpRequest = {
+        params: {
+            userId: faker.string.uuid(),
+        },
+
+        body: {
+            first_name: faker.person.firstName(),
+        },
+    }
+
+    it('should return 200 when updating a user successfully', async () => {
+        // arrange
+        const { sut } = makeSut()
+        // act
+        const test = await sut.execute(httpRequest)
+        // assert
+        expect(test.statusCode).toBe(200)
+    })
+})
