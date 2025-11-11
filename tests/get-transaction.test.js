15a9f8d0fa20e9b6c5afe17045eb74ff72998bfe
Douglas Ramos Pimenta <douglasramospimenta@gmail.com>
2025-11-11 15:55:47 -0300

    test: should GetTransactionController  return correct status code

---
+++ tests/get-transaction.test.js
@@ -0,0 +1,89 @@
+import { faker } from '@faker-js/faker'
+import { GetTransactionByUserIdController } from '../src/controllers/transaction/get-transaction-by-user-id'
+import { UserNotFoundError } from '../src/errors/user'
+
+describe('GetTransactionController', () => {
+    class GetTransactionUseCaseStub {
+        async execute() {
+            return {
+                user_id: faker.string.uuid(),
+                name: faker.commerce.productName(),
+                date: faker.date.anytime().toISOString(),
+                amount: Number(faker.finance.amount()),
+                type: 'EXPENSES',
+            }
+        }
+    }
+
+    const makeSut = () => {
+        const getTransactionUseCase = new GetTransactionUseCaseStub()
+        const sut = new GetTransactionByUserIdController(getTransactionUseCase)
+
+        return { sut, getTransactionUseCase }
+    }
+
+    const httpRequest = {
+        query: {
+            userId: faker.string.uuid(),
+        },
+    }
+
+    it('should return 200 when get transaction is successfully', async () => {
+        // arrange
+        const { sut } = makeSut()
+        // act
+        const test = await sut.execute(httpRequest)
+        // assert
+        expect(test.statusCode).toBe(200)
+    })
+
+    it('should return 400 when user id is missing', async () => {
+        // arrange
+        const { sut } = makeSut()
+        // act
+        const test = await sut.execute({
+            query: {
+                ...httpRequest.query,
+                userId: null,
+            },
+        })
+        // assert
+        expect(test.statusCode).toBe(400)
+    })
+
+    it('should return 400 when User id is invalid', async () => {
+        // arrange
+        const { sut } = makeSut()
+        // act
+        const test = await sut.execute({
+            query: {
+                ...httpRequest.query,
+                userId: 'invalid_userId',
+            },
+        })
+        // assert
+        expect(test.statusCode).toBe(400)
+    })
+
+    it('should return 404 when user not found', async () => {
+        // arrange
+        const { sut, getTransactionUseCase } = makeSut()
+        jest.spyOn(getTransactionUseCase, 'execute').mockImplementationOnce(() => {
+            throw new UserNotFoundError()
+        })
+        // act
+        const test = await sut.execute(httpRequest)
+        // assert
+        expect(test.statusCode).toBe(404)
+    })
+
+    it('should return 500 GetTransactionUseCase failed', async () => {
+        // arrange
+        const { sut, getTransactionUseCase } = makeSut()
+        jest.spyOn(getTransactionUseCase, 'execute').mockRejectedValueOnce()
+        // act
+        const test = await sut.execute(httpRequest)
+        // assert
+        expect(test.statusCode).toBe(500)
+    })
+})
