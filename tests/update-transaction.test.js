6233cedc259d3b511f79298ca08bd505960606b1
Douglas Ramos Pimenta <douglasramospimenta@gmail.com>
2025-11-12 10:30:20 -0300

    test: should UpdateTransactionController  return 200 and 400

---
+++ tests/update-transaction.test.js
@@ -0,0 +1,52 @@
+import { UpdateTransactionController } from '../src/controllers/transaction/update-transaction'
+import { faker } from '@faker-js/faker'
+
+describe('UpdateTransactionController', () => {
+    class UpdateTransactionUseCaseStub {
+        async execute() {
+            return {
+                user_id: faker.string.uuid(),
+                id: faker.string.uuid(),
+                name: faker.commerce.productName(),
+                amount: Number(faker.finance.amount()),
+                type: 'EXPENSES',
+            }
+        }
+    }
+
+    // verificar necessidade destas declarações
+    const httpRequest = {
+        params: { transactionId: faker.string.uuid() },
+        body: {
+            name: faker.person.firstName(),
+        },
+    }
+
+    const makeSut = () => {
+        const updateTransactionUseCase = new UpdateTransactionUseCaseStub()
+        const sut = new UpdateTransactionController(updateTransactionUseCase)
+
+        return { sut, updateTransactionUseCase }
+    }
+
+    it('should return 400 when the transaction Id is invalid', async () => {
+        // arrange
+        const { sut } = makeSut()
+        // act
+        const test = await sut.execute({
+            ...httpRequest,
+            params: { transactionId: 'invalid_transactionId' },
+        })
+        // assert
+        expect(test.statusCode).toBe(400)
+    })
+
+    it('should return 200 when the update succesffuly', async () => {
+        // arrange
+        const { sut } = makeSut()
+        // act
+        const test = await sut.execute(httpRequest)
+        // assert
+        expect(test.statusCode).toBe(200)
+    })
+})
