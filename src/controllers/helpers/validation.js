56593941fb11f711dde3bec8da4ea3bdee2a00d7
Douglas Ramos Pimenta <douglasramospimenta@gmail.com>
2025-11-14 10:53:02 -0300

    refactor: comment useless validations

--- src/controllers/helpers/validation.js
+++ src/controllers/helpers/validation.js
@@ -12,40 +12,40 @@ export const checkedIfIdIsValid = (userId) => {
     return validator.isUUID(userId)
 }
 
-export const checkIfIsString = (value) => typeof value === 'string'
+// export const checkIfIsString = (value) => typeof value === 'string'
 
-export const checkedRequiredFields = (params, requiredFields) => {
-    for (const field of requiredFields) {
-        const fieldIsMissing = !params[field]
-        const fieldIsEmpty =
-            checkIfIsString(params[field]) &&
-            validator.isEmpty(params[field], { ignore_whitespace: true })
-        if (fieldIsMissing || fieldIsEmpty) {
-            return {
-                missingField: field,
-                ok: false,
-            }
-        }
-    }
+// export const checkedRequiredFields = (params, requiredFields) => {
+//     for (const field of requiredFields) {
+//         const fieldIsMissing = !params[field]
+//         const fieldIsEmpty =
+//             checkIfIsString(params[field]) &&
+//             validator.isEmpty(params[field], { ignore_whitespace: true })
+//         if (fieldIsMissing || fieldIsEmpty) {
+//             return {
+//                 missingField: field,
+//                 ok: false,
+//             }
+//         }
+//     }
 
-    return {
-        ok: true,
-        missingField: undefined,
-    }
-}
+//     return {
+//         ok: true,
+//         missingField: undefined,
+//     }
+// }
 
-export const checkIfAmountIsValid = (amount) => {
-    if (typeof amount !== 'number' || isNaN(amount)) {
-        return false
-    }
+// export const checkIfAmountIsValid = (amount) => {
+//     if (typeof amount !== 'number' || isNaN(amount)) {
+//         return false
+//     }
 
-    return validator.isCurrency(amount.toFixed(2), {
-        digits_after_decimal: [2],
-        allow_negatives: false,
-        decimal_separator: '.',
-    })
-}
+//     return validator.isCurrency(amount.toFixed(2), {
+//         digits_after_decimal: [2],
+//         allow_negatives: false,
+//         decimal_separator: '.',
+//     })
+// }
 
-export const checkIfTypeIsValid = (type) => {
-    return ['EARNINGS', 'EXPENSES', 'INVESTMENTS'].includes(type)
-}
+// export const checkIfTypeIsValid = (type) => {
+//     return ['EARNINGS', 'EXPENSES', 'INVESTMENTS'].includes(type)
+// }
