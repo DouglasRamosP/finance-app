0f4b87b6e101e6307be94f97599eb321b0054d2d
Douglas Ramos Pimenta <douglasramospimenta@gmail.com>
2025-11-18 16:33:59 -0300

    test: ensure GetUserBalanceUseCase calls GetUserByIdRepository with correct params

--- tests/use-case/get-user-balance.test.js
+++ tests/use-case/get-user-balance.test.js
@@ -69,4 +69,20 @@ describe('GetBalanceUseCase', () => {
         // assert
         await expect(promise).rejects.toThrow(new UserNotFoundError(userId))
     })
+
+    it('should call GetUserByIdRepository with correct params', async () => {
+        // arrange
+        const { sut, getUserByIdRepository } = makeSut()
+        const userId = faker.string.uuid()
+        const getUserByIdRepositorySpy = jest.spyOn(
+            getUserByIdRepository,
+            'execute',
+        )
+
+        // act
+        await sut.execute(userId)
+
+        // assert
+        expect(getUserByIdRepositorySpy).toHaveBeenCalledWith(userId)
+    })
 })
