4c79a8b2da2b9add42ddb89f2334ead7d52d6776
Douglas Ramos Pimenta <douglasramospimenta@gmail.com>
2025-11-19 14:48:14 -0300

    style: reorganize lines of code and improve formatting

--- src/user-case/transaction/delete-transaction.js
+++ src/user-case/transaction/delete-transaction.js
@@ -1,9 +1,13 @@
 export class DeleteTransactionUseCase {
     constructor(postgresDeleteTransactionRepository) {
-        this.postgresDeleteTransactionRepository = postgresDeleteTransactionRepository
+        this.postgresDeleteTransactionRepository =
+            postgresDeleteTransactionRepository
     }
-    async execute(transactionId){
-        const deleteTransaction = await this.postgresDeleteTransactionRepository.execute(transactionId)
+    async execute(transactionId) {
+        const deleteTransaction =
+            await this.postgresDeleteTransactionRepository.execute(
+                transactionId,
+            )
 
         return deleteTransaction
     }
