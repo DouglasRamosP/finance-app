be58b293071b8c1b79160674197e8e1578f8fc0f
Douglas Ramos Pimenta <douglasramospimenta@gmail.com>
2025-11-24 16:25:23 -0300

    test: ensure POST /api/users returns 201 when user is created

--- prisma/prisma.js
+++ prisma/prisma.js
@@ -1,4 +1,9 @@
-import pkg from '../src/generated/prisma/index.js'
-const { PrismaClient } = pkg
+// import pkg from '../src/generated/prisma/index.js'
+// const { PrismaClient } = pkg
+
+// export const prisma = new PrismaClient()
+
+import { PrismaClient } from '../src/generated/prisma/index.js'
 
 export const prisma = new PrismaClient()
+
