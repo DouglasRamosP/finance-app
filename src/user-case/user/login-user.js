6bb115b27a7a585ab6a983eaf9eea93a33eb9a0e
Douglas Ramos Pimenta <douglasramospimenta@gmail.com>
2025-12-02 16:44:30 -0300

    feat: add LoginUserUseCase

---
+++ src/user-case/user/login-user.js
@@ -0,0 +1,32 @@
+import { InvalidPasswordError, UserNotFoundError } from '../../errors/user.js'
+
+export class LoginUserUseCase {
+    constructor(
+        postgresGetUserByEmailRepository,
+        passwordComparatorAdapter,
+        tokensGeneratorAdapter,
+    ) {
+        this.postgresGetUserByEmailRepository = postgresGetUserByEmailRepository
+        this.passwordComparatorAdapter = passwordComparatorAdapter
+        this.tokensGeneratorAdapter = tokensGeneratorAdapter
+    }
+    async execute(email, password) {
+        // verificar se existe usuário com o email passado
+        const user = await this.postgresGetUserByEmailRepository.execute(email)
+        if (!user) {
+            throw new UserNotFoundError()
+        }
+        // verificar se a senha informada corresponde com com a senha atrelada ao usuário dono do email informado.
+        const passwordIsValid = this.passwordComparatorAdapter.execute(
+            password,
+            user.password,
+        )
+        if (!passwordIsValid) {
+            throw new InvalidPasswordError()
+        }
+        // gerar token de autenticação
+        const tokens = await this.tokensGeneratorAdapter.execute(user.id)
+
+        return { ...user, tokens }
+    }
+}
