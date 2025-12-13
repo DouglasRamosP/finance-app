ff9132101b982519b1c7f281b9ef4c028568abfd
Douglas Ramos Pimenta <douglasramospimenta@gmail.com>
2025-12-13 14:44:50 -0300

    test: ensure GetUserBalanceRepository recives from and to params

--- src/repositories/postgres/user/get-user-balance.js
+++ src/repositories/postgres/user/get-user-balance.js
@@ -2,55 +2,56 @@ import { Decimal } from '@prisma/client/runtime/library'
 import { prisma } from '../../../../prisma/prisma.js'
 
 export class PostgresGetUserBalanceRepository {
-    async execute(userId) {
-        const {
-            _sum: { amount: totalExpenses },
-        } = await prisma.transactions.aggregate({
-            where: {
-                user_id: userId,
-                type: 'EXPENSES',
+    async execute(userId, from, to) {
+        const [
+            {
+                _sum: { amount: totalExpenses },
             },
-            _sum: {
-                amount: true,
+            {
+                _sum: { amount: totalEarnings },
             },
-        })
+            {
+                _sum: { amount: totalInvestments },
+            },
+        ] = await Promise.all([
+            prisma.transactions.aggregate({
+                where: {
+                    user_id: userId,
+                    type: 'EXPENSES',
+                    date: { gte: from, lte: to },
+                },
+                _sum: { amount: true },
+            }),
+            prisma.transactions.aggregate({
+                where: {
+                    user_id: userId,
+                    type: 'EARNINGS',
+                    date: { gte: from, lte: to },
+                },
+                _sum: { amount: true },
+            }),
+            prisma.transactions.aggregate({
+                where: {
+                    user_id: userId,
+                    type: 'INVESTMENTS',
+                    date: { gte: from, lte: to },
+                },
+                _sum: { amount: true },
+            }),
+        ])
 
-        const {
-            _sum: { amount: totalEarnings },
-        } = await prisma.transactions.aggregate({
-            where: {
-                user_id: userId,
-                type: 'EARNINGS',
-            },
-            _sum: {
-                amount: true,
-            },
-        })
+        const _totalEarnings = totalEarnings ?? new Decimal(0)
+        const _totalExpenses = totalExpenses ?? new Decimal(0)
+        const _totalInvestments = totalInvestments ?? new Decimal(0)
 
-        const {
-            _sum: { amount: totalIvestments },
-        } = await prisma.transactions.aggregate({
-            where: {
-                user_id: userId,
-                type: 'INVESTMENTS',
-            },
-            _sum: {
-                amount: true,
-            },
-        })
-
-        const _totalEarnings = totalEarnings || new Decimal(0)
-        const _totalExpenses = totalExpenses || new Decimal(0)
-        const _totalIvestments = totalIvestments || new Decimal(0)
-
-        const balance = new Decimal(
-            _totalEarnings - _totalExpenses - _totalIvestments,
-        )
+        const balance = _totalEarnings
+            .minus(_totalExpenses)
+            .minus(_totalInvestments)
 
         return {
             earnings: _totalEarnings,
             expenses: _totalExpenses,
-            investments: totalIvestments,
+            investments: _totalInvestments,
             balance,
         }
     }
