ae559346211c1c99741e56045c19e34c185de62d
Douglas Ramos Pimenta <douglasramospimenta@gmail.com>
2025-12-14 17:13:34 -0300

    feat: ensure transaction per transaction is returned on/api/balance route

--- src/repositories/postgres/user/get-user-balance.js
+++ src/repositories/postgres/user/get-user-balance.js
@@ -44,14 +44,33 @@ export class PostgresGetUserBalanceRepository {
         const _totalExpenses = totalExpenses ?? new Decimal(0)
         const _totalInvestments = totalInvestments ?? new Decimal(0)
 
+        const total = _totalEarnings
+            .plus(_totalExpenses)
+            .plus(_totalInvestments)
+
         const balance = _totalEarnings
             .minus(_totalExpenses)
             .minus(_totalInvestments)
+
+        const earningsPercentage = total.equals(0)
+            ? 0
+            : _totalEarnings.dividedBy(total).times(100).toNumber()
+
+        const expensesPercentage = total.equals(0)
+            ? 0
+            : _totalExpenses.dividedBy(total).times(100).toNumber()
+
+        const investmentsPercentage = total.equals(0)
+            ? 0
+            : _totalInvestments.dividedBy(total).times(100).toNumber()
 
         return {
             earnings: _totalEarnings,
             expenses: _totalExpenses,
             investments: _totalInvestments,
+            earningsPercentage,
+            expensesPercentage,
+            investmentsPercentage,
             balance,
         }
     }
